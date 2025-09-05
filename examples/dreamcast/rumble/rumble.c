7842a712d3e6b2523143578cde79021201a691ef
Daniel Fairchild <daniel@viamap.net>
2025-09-05 22:15:49 +0200

    some refractoring and cleanup

--- examples/dreamcast/rumble/rumble.c
+++ examples/dreamcast/rumble/rumble.c
@@ -5,51 +5,43 @@
     Copyright (C) 2008, 2023, 2025 Donald Haase
     Copyright (C) 2024, 2025 Daniel Fairchild
 */
-
 /*
 This example allows you to send raw commands to the rumble accessory (aka
 purupuru).
 */
-
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
 #include <kos/init.h>
-
 #include <dc/maple.h>
 #include <dc/maple/controller.h>
 #include <dc/maple/purupuru.h>
-
 #include <dc/minifont.h>
 #include <dc/video.h>
-
 KOS_INIT_FLAGS(INIT_DEFAULT);
 
 typedef struct {
   purupuru_effect_t effect;
   const char *description;
 } baked_pattern_t;
-
+static const baked_pattern_t catalog[] = {
+  {.effect = {.cont = false, .motor = 1, .fpow = 7, .freq = 26, .inc =  1}, .description = "Basic Thud (simple .5s jolt)"},
+  {.effect = {.cont = true,  .motor = 1, .fpow = 1, .freq =  7, .inc = 49}, .description = "Car Idle (69 Mustang)"},
+  {.effect = {.cont = false, .motor = 1, .fpow = 7, .conv = true, .freq = 21, .inc = 38}, .description = "Car Idle (VW beetle)"},
+  {.effect = {.cont = false, .motor = 1, .fpow = 7, .conv = true, .freq = 57, .inc = 51}, .description = "Eathquake (Vibrate, and fade out)"},
+  {.effect = {.cont = true,  .motor = 1, .fpow = 1, .freq =  40, .inc = 5}, .description = "Helicopter"},
+  {.effect = {.cont = false, .motor = 1, .fpow = 2, .freq =  7, .inc = 0}, .description = "Ship's Thrust (as in AAC)"},
+};
 static size_t catalog_index = 0;
 static int loaded_pattern = -1;
-static const baked_pattern_t catalog[] = {
-    {.effect = {.cont = false, .motor = 1, .fpow = 7, .freq = 26, .inc =  1}, .description = "Basic Thud (simple .5s jolt)"},
-    {.effect = {.cont = true,  .motor = 1, .fpow = 1, .freq =  7, .inc = 49}, .description = "Car Idle (69 Mustang)"},
-    {.effect = {.cont = false, .motor = 1, .fpow = 7, .conv = true, .freq = 21, .inc = 38}, .description = "Car Idle (VW beetle)"},
-    {.effect = {.cont = false, .motor = 1, .fpow = 7, .conv = true, .freq = 57, .inc = 51}, .description = "Eathquake (Vibrate, and fade out)"},
-    {.effect = {.cont = true,  .motor = 1, .fpow = 1, .freq =  40, .inc = 5}, .description = "Helicopter"},
-    {.effect = {.cont = false, .motor = 1, .fpow = 2, .freq =  7, .inc = 0}, .description = "Ship's Thrust (as in AAC)"},
-};
+
 /* motor cannot be 0 (will generate error on official hardware), but we can set
  * everything else to 0 for stopping */
 static const purupuru_effect_t rumble_stop = {.motor = 1};
 static purupuru_effect_t effect = rumble_stop;
-
 static int cursor_pos = 0;
 static uint16_t old_buttons = 0, rel_buttons = 0;
-
 static const char *fieldnames[] = {"cont", "res",  "motor", "bpow", "div",
                                    "fpow", "conv", "freq",  "inc"};
 static const int num_fields = sizeof(fieldnames) / sizeof(fieldnames[0]);
@@ -79,7 +71,7 @@ static inline uint8_t offset2field(int offset) {
   }
 }
 
-static inline void alterFieldByOffset(int offset, int delta) {
+static inline void alter_field_at_offset(int offset, int delta) {
   switch (offset) {
   case 0:
     effect.cont = !effect.cont; // cont
@@ -123,48 +115,39 @@ static inline void alterFieldByOffset(int offset, int delta) {
 }
 
 void redraw_screen() {
-
   vid_clear(0, 0, 0);
   int textpos_x = 128, textpos_y = 32;
   minifont_set_color(0xff, 0xc0, 0x10); /* gold */
   minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
                     "Rumble Accessory Tester");
-
   /* Start drawing the changeable section of the screen */
   textpos_y += 30;
   textpos_x = 10;
-
   minifont_set_color(0, 0, 255); /* Blue */
-  for (int i = 0; i < num_fields; i++) {
+  for (int i = 0; i < num_fields; i++)
     minifont_draw_str(vram_s + (640 * textpos_y) + (textpos_x + 60 * i), 640,
                       fieldnames[i]);
-  }
   textpos_y += 16;
   for (int i = 0; i < num_fields; i++) {
     if (cursor_pos == i)
       minifont_set_color(255, 255, 0); /* Yellow */
     else
       minifont_set_color(255, 255, 255); /* White */
-
-    char buf[16];
+    char buf[16] = {0};
     sprintf(buf, " %u ", offset2field(i));
     minifont_draw_str(vram_s + (640 * textpos_y) + (textpos_x + (60 * i)), 640, buf);
   }
   textpos_y += 20;
   textpos_x = 10;
   minifont_set_color(255, 0, 255); /* Magenta */
-  minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640, "hex value:");
+  minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640, "effect hex value:");
   textpos_x += 85;
-
   char sb[11] = {0};
   sprintf(sb, "0x%08lx", effect.raw);
   minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640, sb);
 
-  /* Draw the bottom half of the screen and finish it up. */
-  minifont_set_color(255, 255, 255); /* White */
   textpos_x = 10;
   textpos_y += 32;
-
   minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640, "Field description:");
   minifont_set_color(255, 0, 0); /* RED */
   minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x + 160, 640, "[");
@@ -174,73 +157,38 @@ void redraw_screen() {
                         (strlen(fieldnames[cursor_pos]) + 1) * 8 + 160,
                     640, "]");
   textpos_y += 16;
-  minifont_set_color(255, 255, 255); /* White */
-
   textpos_x += 20;
+  minifont_set_color(255, 255, 255); /* White */
+  const char *field_descriptions[] = { // note that each description is 2 lines, some empty
+      "Continuous Vibration. When set vibration will continue until stopped", "",
+      
+      "Reserved. Always 0s", "also will not be shown.",
 
-  switch (cursor_pos) {
-  case 0:
-    minifont_draw_str(
-        vram_s + (640 * textpos_y) + textpos_x, 640,
-        "Continuous Vibration. When set vibration will continue until stopped");
-    break;
-  case 2:
-    minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                      "Motor number. 0 will cause an error. 1 is the typical "
-                      "setting. 4-bits.");
-    break;
-  case 3:
-    minifont_draw_str(
-        vram_s + (640 * textpos_y) + textpos_x, 640,
-        "Backward direction (- direction) intensity setting bits.");
-    textpos_y += 16;
-    minifont_draw_str(
-        vram_s + (640 * textpos_y) + textpos_x, 640,
-        "0 stops vibration. Exclusive with .fpow. Field is 3-bits.");
-    break;
-  case 4:
-    minifont_draw_str(
-        vram_s + (640 * textpos_y) + textpos_x, 640,
-        "Divergent vibration. Make the rumble stronger until it stops.");
-    textpos_y += 16;
-    minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                      "Exclusive with .conv.");
-    break;
-  case 5:
-    minifont_draw_str(
-        vram_s + (640 * textpos_y) + textpos_x, 640,
-        "Forward direction (+ direction) intensity setting bits.");
-    textpos_y += 16;
-    minifont_draw_str(
-        vram_s + (640 * textpos_y) + textpos_x, 640,
-        "0 stops vibration. Exclusive with .bpow. Field is 3-bits.");
-    break;
-  case 6:
-    minifont_draw_str(
-        vram_s + (640 * textpos_y) + textpos_x, 640,
-        "Convergent vibration. Make the rumble weaker until it stops.");
-    textpos_y += 16;
-    minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                      "Exclusive with .div.");
-    break;
+      "Motor number. 0 will cause an error. 1 is the typical setting. 4-bits.", "",
 
-  case 7:
-    minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                      "Vibration frequency. For most purupuru the range "
-                      "is 4-59. Field is 8-bits.");
-    break;
-  case 8:
-    minifont_draw_str(
-        vram_s + (640 * textpos_y) + textpos_x, 640,
-        "Vibration inclination period setting bits. Field is 8-bits.");
-    textpos_y += 16;
-    minifont_draw_str(
-        vram_s + (640 * textpos_y) + textpos_x, 640,
-        "Setting .inc == 0 when .conv or .div are set results in error.");
-    break;
-  default:
-    break;
-  }
+      "Backward direction (- direction) intensity setting bits.",
+      "0 stops vibration. Exclusive with .fpow. Field is 3-bits.",
+
+      "Divergent vibration. Make the rumble stronger until it stops.",
+      "Exclusive with .conv.",
+
+      "Forward direction (+ direction) intensity setting bits.",
+      "0 stops vibration. Exclusive with .bpow. Field is 3-bits.",
+
+      "Convergent vibration. Make the rumble weaker until it stops.",
+      "Exclusive with .div.",
+
+      "Vibration frequency. For most purupuru the range is 4-59. Field is 8-bits.", "",
+
+      "Vibration inclination period setting bits. Field is 8-bits.", "",
+
+      "Setting .inc == 0 when .conv or .div are set results in error.", ""
+  };
+  minifont_draw_str(
+    vram_s + (640 * textpos_y) + textpos_x, 640, field_descriptions[cursor_pos * 2]);
+  textpos_y += 16;
+  minifont_draw_str(
+    vram_s + (640 * textpos_y) + textpos_x, 640, field_descriptions[cursor_pos * 2 + 1]);
 
   if (loaded_pattern >= 0) {
     textpos_y = 200;
@@ -252,30 +200,23 @@ void redraw_screen() {
     minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x + 20, 640,
                       catalog[loaded_pattern].description);
   }
-
-  minifont_set_color(255, 255, 255); /* White */
   textpos_y = 360;
   textpos_x = 10;
-  minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                    "Press left/right to switch field.");
-  textpos_y += 16;
-  minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                    "Press up/down to change values.");
-  textpos_y += 16;
-  minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                    "Press A to send effect to rumblepack.");
-  textpos_y += 16;
-  minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                    "Press B to stop rumble.");
-  textpos_y += 16;
-  minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                    "Press X for next baked pattern");
-  textpos_y += 16;
-  minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640, "Press Start to quit.");
-
+  minifont_set_color(255, 255, 255); /* White */
+  const char* instructions[] = {
+      "Press left/right to switch field.",
+      "Press up/down to change values.",
+      "Press A to send effect to rumblepack.",
+      "Press B to stop rumble.",
+      "Press X for next baked pattern",
+      "Press Start to quit."
+  };
+  for (size_t i = 0; i < sizeof(instructions) / sizeof(instructions[0]); i++) {
+    minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640, instructions[i]);
+    textpos_y += 16;
+  }
   vid_flip(-1);
 }
-
 /* This blocks waiting for a specified device to be present and valid */
 void wait_for_dev_attach(maple_device_t **dev_ptr, unsigned int func) {
   maple_device_t *dev = *dev_ptr;
@@ -294,12 +235,17 @@ void wait_for_dev_attach(maple_device_t **dev_ptr, unsigned int func) {
     vid_clear(0, 0, 0);
     int textpos_x = 40, textpos_y = 200;
     /* Draw up a screen */
-    if (func == MAPLE_FUNC_CONTROLLER)
-      minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                        "Please attach a controller to port A!");
-    else if (func == MAPLE_FUNC_PURUPURU)
-      minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
-                        "Please attach a rumbler to controller in port A!");
+    switch (func) {
+      case MAPLE_FUNC_CONTROLLER:
+        minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
+                          "Please attach a controller to port A!");
+        break;
+      case MAPLE_FUNC_PURUPURU:
+        minifont_draw_str(vram_s + (640 * textpos_y) + textpos_x, 640,
+                          "Please attach a rumbler to controller in port A!");
+      default:
+        break;
+    }
     vid_flip(-1);
   }
 }
@@ -321,68 +267,49 @@ int main(int argc, char *argv[]) {
 
     /* Store current button states + buttons which have been released. */
     state = (cont_state_t *)maple_dev_status(contdev);
-
     /* Make sure we can rely on the state, otherwise loop. */
     if (state == NULL)
       continue;
 
     rel_buttons = (old_buttons ^ state->buttons);
-
     if ((state->buttons & CONT_DPAD_LEFT) && (rel_buttons & CONT_DPAD_LEFT)) {
       cursor_pos = cursor_pos - 1;
-      if (cursor_pos < 0) {
+      if (cursor_pos < 0)
         cursor_pos = num_fields - 1;
-      }
-      if (cursor_pos == 1) {
+      if (cursor_pos == 1)
         cursor_pos = 0;
-      }
     }
-
     if ((state->buttons & CONT_DPAD_RIGHT) && (rel_buttons & CONT_DPAD_RIGHT)) {
       cursor_pos = (cursor_pos + 1) % num_fields;
-      if (cursor_pos == 1) {
-        cursor_pos = (cursor_pos + 1) % num_fields;
-      }
+      if (cursor_pos == 1)
+        cursor_pos = 2;
     }
-
-    if ((state->buttons & CONT_DPAD_UP)) {
-      alterFieldByOffset(cursor_pos, 1);
+    int delta = (state->buttons & CONT_DPAD_UP) ? 1 : ((state->buttons & CONT_DPAD_DOWN) ? -1 : 0);
+    if ( delta ) {
+      alter_field_at_offset(cursor_pos, delta);
       loaded_pattern = -1;
       usleep(100000); /* 1/10th second */
     }
-
-    if ((state->buttons & CONT_DPAD_DOWN)) {
-      alterFieldByOffset(cursor_pos, -1);
-      loaded_pattern = -1;
-      usleep(100000); /* 1/10th second */
-    }
-
     if ((state->buttons & CONT_X) && (rel_buttons & CONT_X)) {
       effect = catalog[catalog_index].effect;
       loaded_pattern = catalog_index;
-
       catalog_index++;
-      if (catalog_index >= sizeof(catalog) / sizeof(baked_pattern_t)) {
+      if (catalog_index >= sizeof(catalog) / sizeof(baked_pattern_t))
         catalog_index = 0;
-      }
     }
-
     if ((state->buttons & CONT_A) && (rel_buttons & CONT_A)) {
       /* We print these out to make it easier to track the options chosen */
       printf("Rumble effect hex code: 0x%lx!\n", effect.raw);
       purupuru_rumble(purudev, &effect);
     }
-
     if ((state->buttons & CONT_B) && (rel_buttons & CONT_B)) {
       purupuru_rumble(purudev, &rumble_stop);
       printf("Rumble Stopped!\n");
     }
     old_buttons = state->buttons;
   }
-
   /* Stop rumbling before exiting, if it still exists. */
   if ((purudev != NULL) && purudev->valid)
     purupuru_rumble(purudev, &rumble_stop);
-
   return 0;
 }
